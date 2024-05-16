9389be11156499fac818334b7902ea8820278dcd
Ameena Khanum <ameenakhanum@Ameenas-MacBook-Pro.local>
2024-05-16 18:47:30 +0530

    search

--- src/CountryList.js
+++ src/CountryList.js
@@ -1,11 +1,11 @@
-// src/components/CountryList.js
-
 import React, { useState, useEffect } from 'react';
 import './CountryList.css';
 
 const CountryList = () => {
   const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
   const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchCountries = async () => {
@@ -16,6 +16,7 @@ const CountryList = () => {
         }
         const data = await response.json();
         setCountries(data);
+        setFilteredCountries(data); // Initialize filtered countries with all countries
       } catch (error) {
         console.error('Error fetching countries:', error.message);
         setError('Failed to fetch countries');
@@ -25,21 +26,41 @@ const CountryList = () => {
     fetchCountries();
   }, []);
 
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchTerm)
+    );
+    setFilteredCountries(filtered);
+  };
+
+  const renderCountryCards = () => {
+    return filteredCountries.map((country) => (
+      <div key={country.name.common} className="country-card">
+        <img
+          src={country.flags.png}
+          alt={`Flag of ${country.name.common}`}
+          className="country-flag"
+        />
+        <span className="country-name">{country.name.common}</span>
+      </div>
+    ));
+  };
+
   return (
     <div className="country-list">
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={handleSearch}
+        className="search-input"
+      />
       {error ? (
         <div className="error-message">{error}</div>
       ) : (
-        countries.map((country) => (
-          <div key={country.name.common} className="country-card">
-            <img
-              src={country.flags.svg}
-              alt={`Flag of ${country.name.common}`}
-              className="country-flag"
-            />
-            <span className="country-name">{country.name.common}</span>
-          </div>
-        ))
+        <div className="country-cards-container">{renderCountryCards()}</div>
       )}
     </div>
   );
